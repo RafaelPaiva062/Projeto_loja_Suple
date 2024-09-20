0f763a6da469d83d7c6e9ebe879caacafd550519
Rafael Paiva <rafaelpaiva062@gmail.com>
2024-09-20 17:28:33 -0300

    Add files via upload

---
+++ Projeto_Loja/Projeto_loja_Suple/Loja_Suple/src/projetoSupplements/projetoSupplements/AppSuper.java
@@ -0,0 +1,15 @@
+package projetoSupplements;
+import java.util.Scanner;
+
+public class AppSuper {
+
+
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         Prateleiras prateleiras = new Prateleiras();
+        prateleiras.listtodosHiper();
+         
+
+   }
+    
+}
